52ed1f5c29d73244c8608a511035ef74698de100
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-07-30 19:24:11 +0200

    CORS headers

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 mongoose.connect(process.env.MONGODB_URL,
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
