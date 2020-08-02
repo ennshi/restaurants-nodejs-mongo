05464aab5f0eec48ba4ce25716569c13d1dd50d4
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-02 23:51:05 +0200

    Password validator created

--- middlewares/validation/helpers.js
+++ middlewares/validation/helpers.js
@@ -9,3 +9,8 @@ exports.isValidEmail = (trimmedEmail) => {
     const regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regexp.test(trimmedEmail);
 };
+
+exports.isStrongPassword = (trimmedPassword) => {
+    const regexp = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+    return regexp.test(trimmedPassword);
+};
