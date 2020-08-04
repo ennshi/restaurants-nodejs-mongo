ad4e85fe4bc0491da12d9cb453697144d484bd07
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-05 01:05:03 +0200

    loginAdmin created in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const Admin = require('../models/admin');
 
 exports.loginUser = (req, res, next) => {
     const { email, password } = req.body;
@@ -27,6 +28,36 @@ exports.loginUser = (req, res, next) => {
                 {expiresIn: '1h'}
                 );
             res.status(200).json({ token, userId: currentUser._id });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+exports.loginAdmin = (req, res, next) => {
+    const { identifier, password } = req.body;
+    let loggedAdmin;
+    Admin.findOne({ identifier })
+        .then(admin => {
+            if(!admin) {
+                return res.status(404).json({message: "Identifier isn't registered or password is incorrect"});
+            }
+            loggedAdmin = admin;
+            return bcrypt.compare(password, admin.password);
+        })
+        .then(isEqual => {
+            if(!isEqual) {
+                return res.status(404).json({message: "Identifier isn't registered or password is incorrect"});
+            }
+            const key = process.env.JWT_KEY_A;
+            const token = jwt.sign({
+                    adminId: loggedAdmin._id.toString(),
+                    identifier: loggedAdmin.identifier
+                },
+                key,
+                {expiresIn: '1h'}
+            );
+            res.status(200).json({ token, adminId: loggedAdmin._id });
         })
         .catch(err => {
             console.log(err);
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const authController = require('../controllers/auth');
 
 router.post('/login', authController.loginUser);
-// router.post('/login-admin');
+router.post('/login-admin', authController.loginAdmin);
 
 module.exports = router;
