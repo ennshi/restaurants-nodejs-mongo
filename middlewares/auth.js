7c274a83cdaed6a51b67a23bb19417e81415ca7b
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-05 19:20:39 +0200

    adminAuth middleware created

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -13,6 +13,19 @@ exports.userAuth = (req, res, next) => {
     next();
 };
 
+exports.adminAuth = (req, res, next) => {
+    const authHeader = req.get('Authorization');
+    if(!authHeader) {
+        return res.status(401).json({message: 'Authorization failed'});
+    }
+    const decodedToken = decodeToken(authHeader, process.env.JWT_KEY_A);
+    if(!decodedToken) {
+        return res.status(401).json({message: 'Authorization failed'});
+    }
+    req.adminId = decodedToken.adminId;
+    next();
+};
+
 const decodeToken = (authHeader, key) => {
     const token = authHeader.split(' ')[1];
     let decodedToken;
