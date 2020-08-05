9973a77529d30d246114b6b7a496ec81d06842c7
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-05 19:01:31 +0200

    userAuth implemented in user routes

--- routes/user.js
+++ routes/user.js
@@ -6,18 +6,19 @@ const isValidUser = require('../middlewares/validation/user');
 const restaurantController = require('../controllers/restaurant');
 const reviewController = require('../controllers/review');
 const isValidReview = require('../middlewares/validation/review');
+const { userAuth } = require('../middlewares/auth');
 
 router.get('/restaurants', restaurantController.getRestaurants);
 router.get('/restaurants/:restaurantId', restaurantController.getRestaurant);
 
 router.post('/profile', isValidUser, userController.createUser);
-router.get('/profile/:userId', userController.getUser);
-router.put('/profile/:userId', isValidUser, userController.updateUser);
-router.delete('/profile/:userId', userController.deleteUser);
+router.get('/profile', userAuth, userController.getUser);
+router.put('/profile', userAuth, isValidUser, userController.updateUser);
+router.delete('/profile', userAuth, userController.deleteUser);
 
 router.get('/reviews', reviewController.getReviews);
-router.post('/reviews', isValidReview, reviewController.createReview);
-router.put('/reviews/:reviewId', isValidReview, reviewController.updateReview);
-router.delete('/reviews/:reviewId', reviewController.deleteReview);
+router.post('/reviews', userAuth, isValidReview, reviewController.createReview);
+router.put('/reviews/:reviewId', userAuth, isValidReview, reviewController.updateReview);
+router.delete('/reviews/:reviewId', userAuth, reviewController.deleteReview);
 
 module.exports = router;
