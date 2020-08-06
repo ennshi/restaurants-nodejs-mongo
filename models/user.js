a73cf4b77b2d992f24e000150b2ac54bc0f02182
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-06 19:15:24 +0200

    User model fixed

--- models/user.js
+++ models/user.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const Review = require('./review');
+
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     photoUrl: {
         type: String,
@@ -22,11 +27,28 @@ const userSchema = new Schema({
         type: String,
         required: true,
         default: 'active'
-    },
-    reviews: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Review'
-    }]
+    }
 }, { timestamps: true });
+
+userSchema.virtual('reviews', {
+    ref: 'Review',
+    localField: '_id',
+    foreignField: 'creator'
+});
+
+userSchema.methods.toJSON = function() {
+    const user = this;
+    const userObj = user.toObject();
+    delete userObj.password;
+    return userObj;
+};
+
+userSchema.pre('remove', function (next) {
+    const user = this;
+    Review.deleteMany({ creator: user._id })
+        .then(() => {
+            next();
+        })
+});
 
 module.exports = mongoose.model('User', userSchema);
