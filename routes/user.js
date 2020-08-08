2a6b47e18f0450059f3aabf2339604d9d393017c
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-08 19:07:21 +0200

    Avatar route added

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ const restaurantController = require('../controllers/restaurant');
 const reviewController = require('../controllers/review');
 const isValidReview = require('../middlewares/validation/review');
 const { userAuth } = require('../middlewares/auth');
+const { uploadAvatar } = require('../middlewares/image-upload');
 
 router.get('/restaurants', restaurantController.getRestaurants);
 router.get('/restaurants/:restaurantId', restaurantController.getRestaurant);
@@ -15,6 +16,7 @@ router.post('/profile', isValidUser, userController.createUser);
 router.get('/profile', userAuth, userController.getUser);
 router.put('/profile', userAuth, isValidUser, userController.updateUser);
 router.delete('/profile', userAuth, userController.deleteUser);
+router.post('/profile/avatar', userAuth, uploadAvatar, userController.setAvatar);
 
 router.post('/reviews', userAuth, isValidReview, reviewController.createReview);
 router.put('/reviews/:reviewId', userAuth, isValidReview, reviewController.updateReview);
