7e13816f62cb5f379a555da19b9941a1643067e0
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-09 22:51:20 +0200

    Minor fix in errorHandler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,4 +1,4 @@
-module.exports = (error, req, res) => {
+module.exports = (error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
