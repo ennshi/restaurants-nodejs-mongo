fb18503a3d49ca2e32882b6df861488aaca44e70
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-13 13:18:22 +0200

    Geocoder implemented in restaurant model

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const countrycodelookup = require('country-code-lookup');
 const Schema = mongoose.Schema;
 
 const Review = require('./review');
+const geocoder = require('../utils/geocoder');
 
 const restaurantSchema = new Schema({
     name: {
@@ -14,24 +16,21 @@ const restaurantSchema = new Schema({
         required: true,
         trim: true
     },
+    address: {
+        type: String,
+        required: true
+    },
     location: {
-        country: {
+        type: {
             type: String,
-            required: true
+            enum: ['Point']
         },
-        state: {
-            type: String,
-            required: true
+        coordinates: {
+            type: [Number],
+            index: '2dsphere'
         },
-        city: {
-            type: String,
-            required: true
-        },
-        address: {
-            type: String,
-            required: true,
-            trim: true
-        }
+        formattedAddress: String,
+        country: String
     },
     photoUrl: {
         type: String
@@ -42,6 +41,20 @@ restaurantSchema.virtual('reviews', {
     ref: 'Review',
     localField: '_id',
     foreignField: 'restaurant'
+});
+
+restaurantSchema.pre('save', function (next) {
+    geocoder.geocode(this.address)
+        .then(loc => {
+            this.location = {
+                type: 'Point',
+                coordinates: [loc[0].longitude, loc[0].latitude],
+                formattedAddress: loc[0].formattedAddress,
+                country: countrycodelookup.byIso(loc[0].countryCode).country
+            };
+            this.address = undefined;
+            next();
+        });
 });
 
 restaurantSchema.pre('remove', function (next) {
