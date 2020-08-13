0a154f28d2975b8521166cfe46212a84d424defd
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-08-13 13:17:46 +0200

    location routes and controllers deleted

--- routes/location.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const locationController = require('../controllers/location');
-const router = express.Router();
-
-router.get('/countries', locationController.getCountries);
-router.get('/states', locationController.getStates);
-
-module.exports = router;
