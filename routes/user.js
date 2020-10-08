959430c80c20a59f81e336a2ec9c2433f7c105a4
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-10-08 23:33:45 +0200

    User routes fixed

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,8 @@ const isValidReview = require('../middlewares/validation/review');
 const { userAuth } = require('../middlewares/auth');
 const { uploadAvatar } = require('../middlewares/image-upload');
 
-router.get('/restaurants', restaurantController.getRestaurants);
-router.get('/restaurants/:restaurantId', restaurantController.getRestaurant);
+router.get('/restaurant', restaurantController.getRestaurants);
+router.get('/restaurant/:restaurantId', restaurantController.getRestaurant);
 
 router.post('/profile', isValidUser, userController.createUser);
 router.get('/profile', userAuth, userController.getUser);
@@ -18,6 +18,7 @@ router.put('/profile', userAuth, isValidUser, userController.updateUser);
 router.delete('/profile', userAuth, userController.deleteUser);
 router.post('/profile/avatar', userAuth, uploadAvatar, userController.setAvatar);
 
+router.get('/reviews', reviewController.getReviews);
 router.post('/reviews', userAuth, isValidReview, reviewController.createReview);
 router.put('/reviews/:reviewId', userAuth, isValidReview, reviewController.updateReview);
 router.delete('/reviews/:reviewId', userAuth, reviewController.deleteReview);
