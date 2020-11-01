876c52b2e81ae557e35d4640f5fd101227827dbb
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-11-01 23:08:02 +0100

    Restaurant validation num of chars changed

--- middlewares/validation/restaurant.js
+++ middlewares/validation/restaurant.js
@@ -2,11 +2,11 @@ const {isLength} = require('./helpers');
 const isValidRestaurant = (req, res, next) => {
     const errors = {};
     const {name, description, address} = req.body;
-    if(!isLength(name.trim(), {min: 1, max: 255})) {
-        errors.name = "Please provide a name 1-255 characters long";
+    if(!isLength(name.trim(), {min: 1, max: 100})) {
+        errors.name = "Please provide a name 1-100 characters long";
     }
-    if(!isLength(description.trim(), {min: 10, max: 300})) {
-        errors.description = "Please provide a description 10-300 characters long";
+    if(!isLength(description.trim(), {min: 10, max: 400})) {
+        errors.description = "Please provide a description 10-400 characters long";
     }
     if(!isLength(address.trim(), {min: 10, max: 300})) {
         errors.address = "Please provide a valid address 10-300 characters long";
